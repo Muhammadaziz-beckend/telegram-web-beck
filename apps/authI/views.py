3ef70a965ab129c83326035e565fcca3e8b4a99a
Muhammadaziz <muhammadazizdev@gmail.com>
2025-02-13 20:51:55 +0600

    static fieles

--- apps/authI/views.py
+++ apps/authI/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 
 @api_view(['POST'])
 def receive_phone(request):
-    phone_number = request.data.get("phone",None)
+    phone_number = request.data.get("phone")
     if phone_number:
         return Response({"message": f"Ваш номер: {phone_number}"})
     return Response({"error": "Номер не получен"}, status=400)
